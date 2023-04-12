921aa3f94c67ace36c85be4cc153a3e9c1429501
Saikat36 <saikatp360@gmail.com>
2023-04-12 09:23:55 +0530

    added

--- JavaScript/3 - Function/4_opposite_of_push()_&_pop().js
+++ JavaScript/3 - Function/4_opposite_of_push()_&_pop().js
@@ -2,11 +2,11 @@
 
 /* opposite of push() & pop() */
 
-let array1 = [ 2,3,1 ];
+let array1 = [ 2,3,1,8 ];
 
 let x = array1.unshift(7);
 
-console.log(array1);
+console.log(array1);    // push() opposite
 console.log(x);
 
 console.log("\n");
@@ -15,7 +15,7 @@ console.log("\n");
 
 let array2 = [ 2,3,1 ];
 
-let y = array2.shift();
+let y = array2.shift();    // pop() oppsite
 
 console.log(array2);
 console.log(y);
