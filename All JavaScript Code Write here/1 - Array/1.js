910ad841a271f3804fd48e1a0525231a5a26172d
Saikat36 <saikatp360@gmail.com>
2023-10-02 11:22:57 +0530

    added

--- All JavaScript Code Write here/1 - Array/1.js
+++ All JavaScript Code Write here/1 - Array/1.js
@@ -3,16 +3,30 @@
             /*      push() & pop() Method    */
 
 
+
+let animal = ["pigs", "goats", "sheep"];
+let gum = animal.push("horse");
+
+
+console.log(animal);  // [ 'pigs', 'goats', 'sheep', 'horse' ]
+console.log(gum);    // length of the animal arr = 4
+
+
+let x = animal.pop();
+
+console.log(x);          // return only the pop element = horse
+console.log(animal);    // [ 'pigs', 'goats', 'sheep' ]
+
+
+
+
+console.log("\n\n");
+
+
 let num = [1,2,3,4];
 
 num.push(8,9);
 console.log(num);
-
-
-let animal = ["pigs", "goats", "sheep"];
-let gum = animal.push("horse");
-console.log(gum);
-console.log(animal);
 
 
 num.pop();
