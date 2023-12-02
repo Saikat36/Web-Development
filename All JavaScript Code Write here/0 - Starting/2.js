bfb2b021c25cd68d92b0dd5e29e427f0d8e5a474
Saikat36 <saikatp360@gmail.com>
2023-12-02 21:56:04 +0530

    added

---
+++ All JavaScript Code Write here/0 - Starting/2.js
@@ -0,0 +1,15 @@
+
+
+function check(num){
+    
+    if(num%2)
+    {
+        return 12;
+    }
+    else{
+        return 10;
+    }
+}
+
+let x = check(6);
+console.log(x);
---
+++ All JavaScript Code Write here/0 - Starting/3.js
@@ -0,0 +1,15 @@
+
+
+
+// Reverse a string 
+
+
+let str = "masai";
+
+let bag = "";
+
+for(let i=str.length-1; i>=0;i--)
+{
+    bag = bag+str[i];
+}
+console.log(bag);
