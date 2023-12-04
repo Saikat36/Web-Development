7e06885821dea1b536faaac1b8489f4127d555d2
Saikat36 <saikatp360@gmail.com>
2023-12-04 20:50:54 +0530

    added

---
+++ All JavaScript Code Write here/0 - Starting/5.js
@@ -0,0 +1,31 @@
+
+
+
+
+// toString , unary plus operator to convert the string to a number
+
+
+
+
+            /*             toString             */
+
+let x=12;
+let y=45;
+
+console.log(typeof(x),typeof(y));      // number number
+
+let str1=x.toString();
+let str2=y.toString();
+
+console.log(str1+str2);     //  1245
+
+
+
+
+/*                  convert to Number               */
+
+
+let num1 = +str1;   // unary plus operator to convert the string to a number
+let num2 = +str2;   
+
+console.log(num1+num2);     // 57  
---
+++ All JavaScript Code Write here/0 - Starting/6.js
@@ -0,0 +1,9 @@
+
+
+
+let invalidStr = "abc";
+
+let result = +invalidStr;
+
+console.log(typeof result); // Outputs: number
+console.log(result); // Outputs: NaN
